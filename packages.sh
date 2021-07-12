9021184b104d59b820c3bae9beb28a332de373b6
Srirammkm <35733243+Srirammkm@users.noreply.github.com>
2021-07-12 15:26:48 +0530

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -6,7 +6,7 @@ echo "-------------------------"
 echo "Installing AWS CLI"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 apt install unzip
-unzip awscliv2.zip
+unzip awscliv2.zip > /dev/null
 sudo ./aws/install
 echo "-------------------------"
 echo "-------------------------"
